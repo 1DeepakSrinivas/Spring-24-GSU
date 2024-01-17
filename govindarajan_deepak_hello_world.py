71f8c702831aa518f994b3350e37b5dddb47f8e5
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-01-17 10:03:12 -0500

    Create govindarajan_deepak_hello_world.py

---
+++ govindarajan_deepak_hello_world.py
@@ -0,0 +1,4 @@
+print("Hello!")
+print('Our class is held every Tuesday and Thursday at 5:30pm.')
+print("Welcome to CS 1301 Principles of Computer Science I Course!")
+print('Our Lab session is held every Monday at 3:00pm')
