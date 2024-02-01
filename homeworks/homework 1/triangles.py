b87d9f221d74716f34839e76b3ac47755f8a1d5a
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-02-01 17:41:07 -0500

    homework 1

---
+++ homeworks/homework 1/triangles.py
@@ -0,0 +1,32 @@
+import math as m
+def triangles():
+    is_right_angled=False
+    is_acute_angled=False
+    is_obtuse_angled=False
+    side_a=int(input("Please enter the length of side A (in metres): "))
+    side_b=int(input("Please enter the length of side B (in metres): "))
+    side_c=int(input("Please enter the length of side C (in metres): "))
+    perimeter_triangle=side_a+side_b+side_c
+    semi_perimeter=perimeter_triangle/2
+    area_triangle=m.sqrt(semi_perimeter*(semi_perimeter-side_a)*(semi_perimeter-side_b)*(semi_perimeter-side_c)
+
+    if side_a**2+side_b**2==side_c**2 or side_a**2+side_c**2==side_b**2 or side_b**2+side_c**2==side_a**2:
+        is_right_angled=True
+    elif side_a**2+side_b**2>side_c**2 and side_a**2+side_c**2>side_b**2 and side_b**2+side_c**2>side_a**2:
+        is_acute_angled=True
+    elif side_a**2+side_b**2<side_c**2 or side_a**2+side_c**2<side_b**2 or side_b**2+side_c**2<side_a**2:
+        is_obtuse_angled=True
+    else:
+        print("The triangle is invalid.")
+
+    print(f"The perimeter of the triangle is {perimeter_triangle} metres.")
+    print(f'The area of the triangle is {area_triangle} square metres.')
+    if is_right_angled:
+        print("The triangle is a right-angled triangle.")
+    if is_acute_angled:
+        print("The triangle is an acute-angled triangle.")
+    if is_obtuse_angled:
+        print("The triangle is an obtuse-angled triangle.")
+    
+    
+
