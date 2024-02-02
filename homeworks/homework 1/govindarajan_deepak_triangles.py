16f96e7208ec63d893ccfa6359e99405f08cb31c
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-02-01 22:15:48 -0500

    Update govindarajan_deepak_triangles.py

--- homeworks/homework 1/govindarajan_deepak_triangles.py
+++ homeworks/homework 1/govindarajan_deepak_triangles.py
@@ -4,6 +4,7 @@ def triangles():
     is_right_angled=False
     is_acute_angled=False
     is_obtuse_angled=False
+    
     side_a=int(input("Please enter the length of side A (in metres): "))
     side_b=int(input("Please enter the length of side B (in metres): "))
     side_c=int(input("Please enter the length of side C (in metres): "))
@@ -13,11 +14,17 @@ def triangles():
 
     area_triangle=m.sqrt(semi_perimeter*(semi_perimeter-side_a)*(semi_perimeter-side_b)*(semi_perimeter-side_c)) #Heron's formula for area of triangle
 
-    if side_a**2+side_b**2==side_c**2 or side_a**2+side_c**2==side_b**2 or side_b**2+side_c**2==side_a**2: #Pythagorean theorem to check if right-angled
+    if (side_a**2+side_b**2==side_c**2 or 
+        side_a**2+side_c**2==side_b**2 or 
+        side_b**2+side_c**2==side_a**2): #Pythagorean theorem to check if right-angled
         is_right_angled=True
-    elif side_a**2+side_b**2>side_c**2 and side_a**2+side_c**2>side_b**2 and side_b**2+side_c**2>side_a**2: #Cosine rule to check if acute-angled
+    elif (side_a**2+side_b**2>side_c**2 and
+          side_a**2+side_c**2>side_b**2 and 
+          side_b**2+side_c**2>side_a**2): #Cosine rule to check if acute-angled
         is_acute_angled=True
-    elif side_a**2+side_b**2<side_c**2 or side_a**2+side_c**2<side_b**2 or side_b**2+side_c**2<side_a**2: # to check if obtuse-angled
+    elif (side_a**2+side_b**2<side_c**2 or
+          side_a**2+side_c**2<side_b**2 or
+           side_b**2+side_c**2<side_a**2): # to check if obtuse-angled
         is_obtuse_angled=True
     else:
         print("The triangle is invalid.")
@@ -32,3 +39,4 @@ def triangles():
         print("The triangle is an obtuse-angled triangle.")
     
 triangles()
+
