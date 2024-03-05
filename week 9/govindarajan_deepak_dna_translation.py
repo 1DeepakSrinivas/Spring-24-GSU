14a609d932fdaa3b34740aea8982a14519afec09
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-03-05 11:14:56 -0500

    updated changes and integrated import wrt assignment guidelines

--- week 9/govindarajan_deepak_dna_translation.py
+++ week 9/govindarajan_deepak_dna_translation.py
@@ -1,94 +1,5 @@
-amino_acids = {
-    'UUU': 'Phe',
-    'UUC': 'Phe',
-
-    'UUA': 'Leu',
-    'UUG': 'Leu',
-    'CUU': 'Leu',
-    'CUC': 'Leu',
-    'CUA': 'Leu',
-    'CUG': 'Leu',
-
-    'AUU': 'Ile',
-    'AUC': 'Ile',
-    'AUA': 'Ile',
-
-    'AUG': 'Met',
-
-    'GUU': 'Val',
-    'GUC': 'Val',
-    'GUA': 'Val',
-    'GUG': 'Val',
-
-    'UCU': 'Ser',
-    'UCC': 'Ser',
-    'UCA': 'Ser',
-    'UCG': 'Ser',
-    'AGU': 'Ser',
-    'AGC': 'Ser',
-
-    'CCU': 'Pro',
-    'CCC': 'Pro',
-    'CCA': 'Pro',
-    'CCG': 'Pro',
-
-    'ACU': 'Thr',
-    'ACC': 'Thr',
-    'ACA': 'Thr',
-    'ACG': 'Thr',
-
-    'GCU': 'Ala',
-    'GCC': 'Ala',
-    'GCA': 'Ala',
-    'GCG': 'Ala',
-
-    'UAU': 'Tyr',
-    'UAC': 'Tyr',
-
-    'UAA': 'STOP',
-    'UAG': 'STOP',
-
-    'CAU': 'His',
-    'CAC': 'His',
-
-    'CAA': 'Gln',
-    'CAG': 'Gln',
-
-    'AAU': 'Asn',
-    'AAC': 'Asn',
-
-    'AAA': 'Lys',
-    'AAG': 'Lys',
-
-    'GAU': 'Asp',
-    'GAC': 'Asp',
-
-    'GAA': 'Glu',
-    'GAG': 'Glu',
-
-    'UGU': 'Cys',
-    'UGC': 'Cys',
-
-    'UGA': 'STOP',
-
-    'UGG': 'Trp',
-
-    'CGU': 'Arg',
-    'CGC': 'Arg',
-    'CGA': 'Arg',
-    'CGG': 'Arg',
-    'AGA': 'Arg',
-    'AGG': 'Arg',
-
-    'GGU': 'Gly',
-    'GGC': 'Gly',
-    'GGA': 'Gly',
-    'GGG': 'Gly',
-}
-
-import govindarajan_deepak_helper.py as helper
-
-helper.chunk(string, chunk_size)
+import govindarajan_deepak_helper as helper
+from govindarajan_deepak_helper import amino_acids
 
 def transcription(dna_sequence):
     mrna = ""
@@ -107,11 +18,11 @@ def transcription(dna_sequence):
 def translate(mrna):
     protein = ''
     # Split mrna into nucleotide triplets
-    triplets = chunk(mrna, 3)
+    triplets = helper.chunk(mrna, 3)
     
     # Replace Triplets with Amino Acids
     for triplet in triplets:
-        protein += amino_acids[triplet] + " "
+        protein += helper.amino_acids[triplet] + " "
 
     return protein
 
