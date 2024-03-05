63305d5adc8c1a8c6033d655b44a009ffe5f0025
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-03-05 12:29:12 -0500

    Added changes to calculator.py under Homwerok 2

--- homeworks/homework 2/govindarajan_deepak_calculator.py
+++ homeworks/homework 2/govindarajan_deepak_calculator.py
@@ -1,49 +1,56 @@
+import math
 def calculator():
     while True:
         print("Please enter an expression")
         expression = input(":> ")
-        split_exp=expression.split()
         
-        if '+' in split_exp:
-            addition(split_exp[0],split_exp[2])
-        elif '-' in split_exp:
-            subtraction(split_exp[0],split_exp[2])
-        elif '*' in split_exp:
-            multiplication(split_exp[0],split_exp[2])
-        elif '/' in split_exp:
-            division(split_exp[0],split_exp[2])
-        elif '%' in split_exp:
-            modulus(split_exp[0],split_exp[2])
-        elif '**' in split_exp:
-            exponent(split_exp[0],split_exp[2])
-        elif '//' in split_exp:
-            floor_division(split_exp[0],split_exp[2])
+        add_split=expression.split(sep='+')
+        sub_split=expression.split(sep='-')
+        mul_split=expression.split(sep='*')
+        div_split=expression.split(sep='/')
+        mod_split=expression.split(sep='%')
+        exp_split=expression.split(sep='**')
+        floor_split=expression.split(sep='//')
+
+        if '+' in expression:
+            addition(add_split[0],add_split[1])
+        elif '-' in expression:
+            subtraction(sub_split[0],sub_split[1])
+        elif '*' in expression:
+            multiplication(mul_split[0],mul_split[1])
+        elif '/' in expression:
+            division(div_split[0],div_split[1])
+        elif '%' in expression:
+            modulus(mod_split[0],mod_split[1])
+        elif '**' in expression:
+            exponent(exp_split[0],exp_split[1])
+        elif '//' in expression:
+            floor_division(floor_split[0],floor_split[1])
         elif expression.lower() == 'exit' or 'quit' or 'q':
             break
-
         else:
             print("Invalid expression")
     
 def addition(a,b):
-    print(a+b)
+    print(f'Result: {a}+{b} = {int(a)+int(b)}')
 
 def subtraction(a,b):
-    print(a-b)
+    print(f'Result: {a}-{b} = {int(a)-int(b)}')
 
 def multiplication(a,b):
-    print(a*b)
+    print(f'Result: {a}*{b} = {int(a)*int(b)}')
 
 def division(a,b):
-    print(a/b)
+    print(f'Result: {a}/{b} = {int(a)/int(b)}')
 
 def modulus(a,b):
-    print(a%b)
+    print(f'Result: {a}%{b} = {int(a)%int(b)}')
 
 def exponent(a,b):
-    print(a**b)
+    print(f'Result: {a}**{b} = {math.pow(int(a),int(b))}')
 
 def floor_division(a,b):
-    print(a//b)
+    print(f'Result: {a}//{b} = {int(a)//int(b)}')
     
 calculator()
     
