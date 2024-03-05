7b84435cc32682c0bb695870fc731d4ce2e68534
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-03-05 12:03:48 -0500

    Update govindarajan_deepak_calculator.py

--- homeworks/homework 2/govindarajan_deepak_calculator.py
+++ homeworks/homework 2/govindarajan_deepak_calculator.py
@@ -1,4 +1,49 @@
 def calculator():
-    print("Please enter an expression")
-    expression = input(":> ")
+    while True:
+        print("Please enter an expression")
+        expression = input(":> ")
+        split_exp=expression.split()
+        
+        if '+' in split_exp:
+            addition(split_exp[0],split_exp[2])
+        elif '-' in split_exp:
+            subtraction(split_exp[0],split_exp[2])
+        elif '*' in split_exp:
+            multiplication(split_exp[0],split_exp[2])
+        elif '/' in split_exp:
+            division(split_exp[0],split_exp[2])
+        elif '%' in split_exp:
+            modulus(split_exp[0],split_exp[2])
+        elif '**' in split_exp:
+            exponent(split_exp[0],split_exp[2])
+        elif '//' in split_exp:
+            floor_division(split_exp[0],split_exp[2])
+        elif expression.lower() == 'exit' or 'quit' or 'q':
+            break
+
+        else:
+            print("Invalid expression")
+    
+def addition(a,b):
+    print(a+b)
+
+def subtraction(a,b):
+    print(a-b)
+
+def multiplication(a,b):
+    print(a*b)
+
+def division(a,b):
+    print(a/b)
+
+def modulus(a,b):
+    print(a%b)
+
+def exponent(a,b):
+    print(a**b)
+
+def floor_division(a,b):
+    print(a//b)
+    
+calculator()
     
