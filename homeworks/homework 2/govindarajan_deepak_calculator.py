0d199a8848a68f28eda283e9b8b7c2abca065a69
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-03-06 15:50:36 -0500

    Homework 2

--- homeworks/homework 2/govindarajan_deepak_calculator.py
+++ homeworks/homework 2/govindarajan_deepak_calculator.py
@@ -1,4 +1,3 @@
-import math
 def calculator():
     while True:
         print("Please enter an expression")
@@ -14,19 +13,19 @@ def calculator():
 
         if '+' in expression:
             addition(add_split[0],add_split[1])
-        if '-' in expression:
+        elif '-' in expression:
             subtraction(sub_split[0],sub_split[1])
-        if '*' in expression:
-            multiplication(mul_split[0],mul_split[1])
-        if '/' in expression:
-            division(div_split[0],div_split[1])
-        if '%' in expression:
-            modulus(mod_split[0],mod_split[1])
-        if '**' in expression:
+        elif '**' in expression:
             exponent(exp_split[0],exp_split[1])
-        if '//' in expression:
+        elif '*' in expression:
+            multiplication(mul_split[0],mul_split[1])
+        elif '//' in expression:
             floor_division(floor_split[0],floor_split[1])
-        if expression.lower() == 'exit' or 'quit' or 'q':
+        elif '/' in expression:
+            division(div_split[0],div_split[1])
+        elif '%' in expression:
+            modulus(mod_split[0],mod_split[1])
+        elif expression.lower() == 'exit' or 'quit' or 'q':
             break
         elif len(expression)<3:
             print("Invalid expression")
@@ -47,9 +46,10 @@ def modulus(a,b):
     print(f'Result: {a}%{b} = {int(a)%int(b)}')
 
 def exponent(a,b):
-    print(f'Result: {a}**{b} = {math.pow(int(a),int(b))}')
+    print(f'Result: {a}**{b} = {(int(a)**int(b))}')
 
 def floor_division(a,b):
+    
     print(f'Result: {a}//{b} = {int(a)//int(b)}')
     
 calculator()
