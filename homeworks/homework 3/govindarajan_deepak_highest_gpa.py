09f2dcab2a489a92fc0eeb73e44e4e27f33e57e8
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-04-05 12:54:18 -0400

    Added changes to main()

--- homeworks/homework 3/govindarajan_deepak_highest_gpa.py
+++ homeworks/homework 3/govindarajan_deepak_highest_gpa.py
@@ -24,14 +24,20 @@ class Course():
     def course_size(self):
         return len(self.roster)
 
-    def find_student_highest_gpa():
+    def find_student_highest_gpa(self):
+        if not self.roster:
+            raise EmptyRosterError
         max_gpa = 0
+        highest_gpa_student = None
         for student in self.roster:
             if student.get_gpa() > max_gpa:
                 max_gpa = student.get_gpa()
                 highest_gpa_student = student
         return highest_gpa_student
 
+class EmptyRosterError(Exception):
+    def error(self):
+        return "Exception. Course Roster is Empty."
 
 def main():
     course = Course([])
@@ -39,7 +45,14 @@ def main():
     while True:
         print("Please Add Students to the Course: (quit or q to exit)\n\n")
         first_name = input("Enter First Name: ")
-        if first_name() in ['quit', 'q']:
+        if first_name.lower() in ['quit', 'q']:
+            if course.course_size() > 0:
+                try:
+                    highest_gpa_student = course.find_student_highest_gpa()
+                    print(f"Student with highest GPA: {highest_gpa_student.get_first()} {highest_gpa_student.get_last()}, GPA: {highest_gpa_student.get_gpa()}")
+                except EmptyRosterError as e:
+                    print(e)
+            print(f"Number of students enrolled: {course.course_size()}")
             break
         last_name = input("Enter Last Name: ")
         gpa = input("Enter GPA: ")
@@ -48,11 +61,8 @@ def main():
             course.add_student(student)
         except ValueError:
             print("Error. Enter a Numeric GPA.")
-    print(f"Number of students enrolled: {course.course_size()}")
-    try:
-        highest_gpa_student = course.find_student_highest_gpa()
-        print(f"Student with highest GPA: {highest_gpa_student.get_first()} {highest_gpa_student.get_last()}, GPA: {highest_gpa_student.get_gpa()}")
-    except EmptyRosterError as e:
-        print('Exception: Course Roster is Empty.')
 
 main()
+        
+        
+        
