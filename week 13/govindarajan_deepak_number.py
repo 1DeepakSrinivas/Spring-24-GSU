87d43f67ed846c938f205402ba09171fd16000ba
Deepak Srinivas <90671142+1DeepakSrinivas@users.noreply.github.com>
2024-04-08 21:53:00 -0400

    created number.png

--- week 13/govindarajan_deepak_number.py
+++ week 13/govindarajan_deepak_number.py
@@ -6,41 +6,47 @@ class Number:
         return str(self.num)
 
     def __add__(self, other):
-        if isinstance(other, Number):
+        try:
             return Number(self.num + other.num)
-        else:
+        except AttributeError:
             raise TypeError("Other object must be of type Number")
 
     def __sub__(self, other):
-        if isinstance(other, Number):
+        try:
             return Number(self.num - other.num)
-        else:
+        except AttributeError:
             raise TypeError("Other object must be of type Number")
 
     def __mul__(self, other):
-        if isinstance(other, Number):
+        try:
             return Number(self.num * other.num)
-        else:
+        except AttributeError:
             raise TypeError("Other object must be of type Number")
 
     def __truediv__(self, other):
-        if isinstance(other, Number):
+        try:
             if other.num != 0:
                 return Number(self.num // other.num)
             else:
                 raise ZeroDivisionError("Cannot divide by zero")
-        else:
+        except AttributeError:
             raise TypeError("Other object must be of type Number")
-
 
 num1 = Number(25)
 num2 = Number(5)
 
-print(f'{num1} + {num2} = {str(num1 + num2)}')
-print(f'{num1} - {num2} = {str(num1 - num2)}')
-print(f'{num1} * {num2} = {str(num1 * num2)}')
-print(f'{num1} / {num2} = {str(num1 / num2)}')
+result_add = num1 + num2
+print(f"{num1} + {num2} = {result_add}")
 
-num3 = ((num1 + num2) * num1) / (num2 * num1)
-print(f'({num1} + {num2}) - {num1}) * {num2 / num1} = {str(num3)}')
+result_sub = num1 - num2
+print(f"{num1} - {num2} = {result_sub}")
+
+result_mul = num1 * num2
+print(f"{num1} * {num2} = {result_mul}")
+
+result_div = num1 / num2
+print(f"{num1} / {num2} = {result_div}")
+
+result_comb=(result_add+result_sub*result_div)/result_mul
+print(f"({result_add} + {result_sub} * {result_div}) / {result_mul} = {result_comb}")
 
